cac88fa9c535aeabba7d12110bdfc1a7afade987
Your Name <brandonks337@gmail.com>
2023-08-28 16:23:26 -0500

    pushing battlepit render to git. Needs work but the basics are almost there. Can incorperate the chat.io for sure but the functionality of the combat will have to wait.

--- Client/src/components/pages/BattlePit.jsx
+++ Client/src/components/pages/BattlePit.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import "../styles/BattlePit.css";
+
+import sendIcon from "../assets/quill_send.svg"; // Import the send icon
 
 export const BattlePit = () => {
-  return <h1>The Battle Pitt</h1>;
+  return (
+    <div className="arena-container">
+      <h1>The Battle Pitt</h1>
+      <div className="encounters-container">
+        <div className="far-left">
+          <button className="allies-container">Click to Add Allies</button>
+        </div>
+        <div className="middle">
+          <button className="enemies-container">Click to Add Enemies</button>
+        </div>
+        <div className="far-right">
+          <div className="results-chat-container">Roll/racking //chat container</div>
+          <div className="encounter-tracker">
+            <div className="chatbox-container">
+              <input type="text" className="chatbox" placeholder="I'm the chatbox" />
+              <button className="icon">
+                <img src={sendIcon} alt="Send" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
