cc23d5fa9fc363359bf6521bff689ca59314aa7f
Project V2 <brandonks337@gmail.com>
2023-08-28 22:41:40 -0500

    added 'window default' confirmation on attempt to delete. can do something fancier later but for now this works as a stop gap from just insta nuking the characters out of the db. Also, discovered while working on the edit buttons error that it was the way I was taking in the state. Changed the state to reference differently and then ran into ANOTHER issue because evidently INT doesnt just go forever. Had to change the hero model to BIGINT to handle larger numbers. This won't technically be an issue for newer players but is good to get out of the way early. Need to keep it in mind for the other parts of the project just in case my other integers need changed aswell.

--- Client/src/components/pages/Characters.jsx
+++ Client/src/components/pages/Characters.jsx
@@ -7,7 +7,7 @@ export const Characters = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [showOverlay, setShowOverlay] = useState(false);
   const [flippedCards, setFlippedCards] = useState({});
-  const [isEditing, setIsEditing] = useState(false);
+  const [editingHeroId, setEditingHeroId] = useState(null);
   const [editedCharacter, setEditedCharacter] = useState({});
 
   const toggleOverlay = () => {
@@ -33,9 +33,8 @@ export const Characters = () => {
     setSearchTerm(event.target.value);
   };
 
-  const handleCardClick = (hero_id) => {
-    //adding checks to make sure this part doesn't flip the dang card if I have it in "edit" mode.....please lord work
-    if (!isEditing) {
+  const handleCardClick = (hero_id, isCurrentCardEditing) => {
+    if (!isCurrentCardEditing) {
       setFlippedCards((prevFlips) => ({
         ...prevFlips,
         [hero_id]: !prevFlips[hero_id],
@@ -69,6 +68,19 @@ export const Characters = () => {
   };
 
   const deleteCharacter = async (hero_id) => {
+    //adding in confirmation for delete option prior to sending fetch request
+    const character = characters.find((char) => char.hero_id === hero_id);
+    const characterName = character ? character.hero_name : "this character";
+
+    const userConfirmed = window.confirm(
+      `Are you sure you want to delete ${characterName}?`
+    );
+
+    if (!userConfirmed) {
+      return; //at this point if user did not "confirm" deletion the function stops closing the warning.
+    }
+    //if user clicks yes....proceeds to try statement and runs delete through delete method
+
     try {
       const response = await fetch(
         `http://localhost:8000/api/heroes/${hero_id}`,
@@ -82,7 +94,7 @@ export const Characters = () => {
         return;
       }
 
-      //if that works filter out deleted hero from the array
+      // if that works filter out deleted hero from the array
       const updatedCharacters = characters.filter(
         (character) => character.hero_id !== hero_id
       );
@@ -115,138 +127,166 @@ export const Characters = () => {
         </div>
       </div>
       <div className="characterCards">
-        {filteredCharacters.map((character) => (
-          <div
-            key={character.hero_id}
-            className={`character-card ${
-              flippedCards[character.hero_id] ? "flipped" : ""
-            }`}
-            onClick={() => handleCardClick(character.hero_id)}
-          >
-            <div className="card-inner">
-              <div
-                className="card-front"
-                style={{ backgroundImage: `url(${character.image_url})` }} //need to set this to a this OR this somehow so that the seeded cards and user stuff can both pull
-                //aka seeded is taking a url from stuff in my assets folder but users will be downloading things as a blob file to the database..... might set as url for them for now and force the change later somehow
-              >
-                <h3>{character.hero_name}</h3>
-              </div>
-              <div className="card-back">
-                <div className="back-top-half">
-                  <h2>
-                    {isEditing ? (
-                      <input
-                        value={editedCharacter.hero_name || ""}
-                        onChange={(e) =>
-                          setEditedCharacter((prevState) => ({
-                            ...prevState,
-                            hero_name: e.target.value,
-                          }))
-                        }
-                      />
-                    ) : (
-                      character.hero_name
-                    )}
-                  </h2>
-                  <div className="level-circle">{character.hero_level}</div>
+        {filteredCharacters.map((character) => {
+          const isCurrentCardEditing = character.hero_id === editingHeroId;
+
+          return (
+            <div
+              key={character.hero_id}
+              className={`character-card ${
+                flippedCards[character.hero_id] ? "flipped" : ""
+              }`}
+              onClick={() =>
+                handleCardClick(character.hero_id, isCurrentCardEditing)
+              }
+            >
+              <div className="card-inner">
+                <div
+                  className="card-front"
+                  style={{ backgroundImage: `url(${character.image_url})` }}
+                >
+                  <h3>{character.hero_name}</h3>
                 </div>
-                <div className="stats edit-section">
-                  <p>
-                    Race:
-                    {isEditing ? (
-                      <input
-                        value={editedCharacter.race_id || ""}
-                        onChange={(e) =>
-                          setEditedCharacter((prevState) => ({
-                            ...prevState,
-                            race_id: e.target.value,
-                          }))
+                <div className="card-back">
+                  <div className="back-top-half">
+                    <h2>
+                      {isCurrentCardEditing ? (
+                        <input
+                          value={editedCharacter.hero_name || ""}
+                          onChange={(e) =>
+                            setEditedCharacter((prevState) => ({
+                              ...prevState,
+                              hero_name: e.target.value,
+                            }))
+                          }
+                        />
+                      ) : (
+                        character.hero_name
+                      )}
+                    </h2>
+                    <div className="level-circle">{character.hero_level}</div>
+                  </div>
+                  <div className="stats edit-section">
+                    <p>
+                      Race:
+                      {isCurrentCardEditing ? (
+                        <input
+                          value={editedCharacter.race_id || ""}
+                          onChange={(e) =>
+                            setEditedCharacter((prevState) => ({
+                              ...prevState,
+                              race_id: e.target.value,
+                            }))
+                          }
+                        />
+                      ) : (
+                        character.race_id
+                      )}
+                    </p>
+                    <p>
+                      Class:
+                      {isCurrentCardEditing ? (
+                        <input
+                          value={editedCharacter.class_id || ""}
+                          onChange={(e) =>
+                            setEditedCharacter((prevState) => ({
+                              ...prevState,
+                              class_id: e.target.value,
+                            }))
+                          }
+                        />
+                      ) : (
+                        character.class_id
+                      )}
+                    </p>
+                    <p>
+                      XP:
+                      {isCurrentCardEditing ? (
+                        <input
+                          type="number"
+                          maxLength="10"
+                          pattern="\d*"
+                          value={editedCharacter.xp || ""}
+                          onChange={(e) => {
+                            if (
+                              e.target.value.length <= 10
+                            ) {
+                              setEditedCharacter((prevState) => ({
+                                ...prevState,
+                                xp: e.target.value,
+                              }));
+                            }
+                          }}
+                        />
+                      ) : (
+                        character.xp
+                      )}
+                    </p>
+                  </div>
+                  <div className="background edit-section">
+                    <p>
+                      <strong>Background: </strong>
+                      {isCurrentCardEditing ? (
+                        <input
+                          value={editedCharacter.background || ""}
+                          onChange={(e) =>
+                            setEditedCharacter((prevState) => ({
+                              ...prevState,
+                              background: e.target.value,
+                            }))
+                          }
+                        />
+                      ) : (
+                        character.background
+                      )}
+                    </p>
+                  </div>
+                  <div className="edit-delete-buttons">
+                    <button
+                      className="edit-button"
+                      onClick={(e) => {
+                        e.stopPropagation(); //prevents the card flip
+                        if (isCurrentCardEditing) {
+                          updateCharacter(character.hero_id);
+                          setEditingHeroId(null);
+                        } else {
+                          setEditingHeroId(character.hero_id);
+                          setEditedCharacter(character);
                         }
-                      />
-                    ) : (
-                      character.race_id
-                    )}
-                  </p>
-                  <p>
-                    Class:
-                    {isEditing ? (
-                      <input
-                        value={editedCharacter.class_id || ""}
-                        onChange={(e) =>
-                          setEditedCharacter((prevState) => ({
-                            ...prevState,
-                            class_id: e.target.value,
-                          }))
-                        }
-                      />
-                    ) : (
-                      character.class_id
-                    )}
-                  </p>
-                  <p>
-                    XP:
-                    {isEditing ? (
-                      <input
-                        value={editedCharacter.xp || ""}
-                        onChange={(e) =>
-                          setEditedCharacter((prevState) => ({
-                            ...prevState,
-                            xp: e.target.value,
-                          }))
-                        }
-                      />
-                    ) : (
-                      character.xp
-                    )}
-                  </p>
-                </div>
-                <div className="background edit-section">
-                  <p>
-                    <strong>Background: </strong>
-                    {isEditing ? (
-                      <input
-                        value={editedCharacter.background || ""}
-                        onChange={(e) =>
-                          setEditedCharacter((prevState) => ({
-                            ...prevState,
-                            background: e.target.value,
-                          }))
-                        }
-                      />
-                    ) : (
-                      character.background
-                    )}
-                  </p>
-                </div>
-                <div className="edit-delete-buttons">
-                  <button
-                    className="edit-button"
-                    onClick={(e) => {
-                      e.stopPropagation(); //prevents the card flip
-                      if (isEditing) {
-                        updateCharacter(character.hero_id);
-                      }
-                      setIsEditing(!isEditing);
-                      if (!isEditing) setEditedCharacter(character); //sets that character to edit mode
-                    }}
-                  >
-                    {isEditing ? "Submit" : "Edit"}
-                  </button>
-                  <button
-                    className="delete-button"
-                    onClick={(e) => {
-                      e.stopPropagation(); //this keeps the card from flipping
-                      deleteCharacter(character.hero_id);
-                    }}
-                  >
-                    Delete
-                  </button>
+                      }}
+                    >
+                      {isCurrentCardEditing ? "Submit" : "Edit"}
+                    </button>
+                    <button
+                      className="delete-button"
+                      onClick={(e) => {
+                        e.stopPropagation(); //this keeps the card from flipping
+                        deleteCharacter(character.hero_id);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
 };
+
+//side note. Saw another post about doing max input characters like this
+/* if (
+    pattern="\d*"
+    added to input portion \d*" d matches any digit and * matches 0 or more of preceding elemnt. is used for input type text  to make sure only numbers are entered.
+
+    e.target.value.length <= 10 &&        // sets max .length value less or = to INT
+    /^[0-9]*$/.test(e.target.value)       // this tests value of input.
+    /^[0-9]*$/ = a regex pattern??
+    ^ asserts position at start of line
+    [0-9] = any digit between 0 and your set max
+    * = quantifier that matches between 0 and unlimited occurrences of preceding element...
+    $ = asserts the position at end of the line
+) */
